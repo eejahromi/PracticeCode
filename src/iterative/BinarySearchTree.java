8e83aa9164f5004e63f231bdeecfdb3d5c65f5f7
Ehsan Jahromi <ehsan.enj@gmail.com>
2016-09-06 16:26:46 -0700

    added iterative pre and inorder traversals

--- src/iterative/BinarySearchTree.java
+++ src/iterative/BinarySearchTree.java
@@ -1,5 +1,12 @@
 package iterative;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+import org.omg.CORBA.Current;
+
 public class BinarySearchTree {
 	private Node root;
 	
@@ -24,16 +31,49 @@ public class BinarySearchTree {
 				current = current.left;
 				if(current == null) {
 					parent.left = newNode;
+					return;
 				}
 			} else if(value > current.data) {
 				current = current.right;
 				if(current == null) {
 					parent.right = newNode;
+					return;
 				}
 			}
 		}
 	}
 	
+	public void inOrderTraversal() {
+		Stack<Node> stack = new Stack<>();
+		Node current = root;
+		ArrayList<Integer> list = new ArrayList<>();
+		while(!stack.isEmpty() || current != null) {
+			if(current != null) {
+				stack.push(current);
+				current = current.left;
+			} else {
+				current = stack.pop();
+				list.add(current.data);
+				current = current.right;
+			}
+		}
+		System.out.print(list);
+	}
+	
+	public void preOrderTraversal() {
+		Deque<Node> path = new LinkedList<>();
+		path.addFirst(root);
+		ArrayList<Integer> result = new ArrayList<>();
+		while(!path.isEmpty()) {
+			Node current = path.removeFirst();
+			if(current != null) {
+				result.add(current.data);
+				path.addFirst(current.right);
+				path.addFirst(current.left);
+			}
+		}
+		System.out.print(result);
+	}
 	
 	
 	class Node {
---
+++ src/iterative/DriverIterative.java
@@ -0,0 +1,16 @@
+package iterative;
+
+public class DriverIterative {
+
+	public static void main(String[] args) {
+		BinarySearchTree tree = new BinarySearchTree();
+		tree.insert(8);
+		tree.insert(6);
+		tree.insert(5);
+		tree.insert(7);
+		
+		tree.inOrderTraversal();
+
+	}
+
+}
