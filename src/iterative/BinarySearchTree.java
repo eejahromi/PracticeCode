934af84d990bc70003b13feb792f35176437d384
Ehsan Jahromi <ehsan.enj@gmail.com>
2016-09-06 15:19:55 -0700

    added iterative insertion

--- src/iterative/BinarySearchTree.java
+++ src/iterative/BinarySearchTree.java
@@ -1,5 +1,49 @@
 package iterative;
 
 public class BinarySearchTree {
+	private Node root;
 	
+	public BinarySearchTree() {
+		root = null;
+	}
+	
+	public void insert(int value) {
+		Node newNode = new Node(value);
+		
+		if(root == null) {
+			root = newNode;
+			return;
+		}
+		
+		Node current = root;
+		Node parent = null;
+		while(true) {
+			parent = current;
+			
+			if(value <= current.data) {
+				current = current.left;
+				if(current == null) {
+					parent.left = newNode;
+				}
+			} else if(value > current.data) {
+				current = current.right;
+				if(current == null) {
+					parent.right = newNode;
+				}
+			}
+		}
+	}
+	
+	
+	
+	class Node {
+		int data;
+		Node left;
+		Node right;
+		
+		public Node(int d) {
+			data = d;
+		}
+	}
 }
+
